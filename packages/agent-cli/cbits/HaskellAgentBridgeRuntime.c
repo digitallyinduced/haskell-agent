93541d28667848391caa1982da9b5a41bd181d2b
Marc Scholten <mpscholten@users.noreply.github.com>
2026-08-30 18:36:31 +0200

    Keep embedded RTS alive for process lifetime

--- packages/agent-cli/cbits/HaskellAgentBridgeRuntime.c
+++ packages/agent-cli/cbits/HaskellAgentBridgeRuntime.c
@@ -5,14 +5,16 @@
 
 static pthread_mutex_t runtime_lock = PTHREAD_MUTEX_INITIALIZER;
 static unsigned int runtime_references = 0;
+static int runtime_initialized = 0;
 
 int32_t ha_runtime_init(void) {
     pthread_mutex_lock(&runtime_lock);
-    if (runtime_references == 0) {
+    if (!runtime_initialized) {
         int argc = 1;
         char *argv[] = {"haskell-agent-macos", NULL};
         char **argv_pointer = argv;
         hs_init(&argc, &argv_pointer);
+        runtime_initialized = 1;
     }
     runtime_references += 1;
     pthread_mutex_unlock(&runtime_lock);
@@ -23,9 +25,6 @@ void ha_runtime_exit(void) {
     pthread_mutex_lock(&runtime_lock);
     if (runtime_references > 0) {
         runtime_references -= 1;
-        if (runtime_references == 0) {
-            hs_exit();
-        }
     }
     pthread_mutex_unlock(&runtime_lock);
 }
